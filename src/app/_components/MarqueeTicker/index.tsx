e8c671fc72bec733259c0ea57bff1cdee1372c5f
starci183 <cuongnvtse160875@gmail.com>
2025-07-25 00:32:05 +0700

    complete

--- src/app/_components/MarqueeTicker/index.tsx
+++ src/app/_components/MarqueeTicker/index.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import { Image } from "@heroui/react"
+import { Image, Spacer } from "@heroui/react"
 import Marquee from "react-fast-marquee"
 import clsx from "clsx"
 
-const images = [
+const imagesTop = [
     {
         src: "/tech/aptos.svg",
         alt: "aptos",
@@ -48,18 +48,67 @@ const images = [
     },
 ]
 
+const imagesBottom = [
+    {
+        src: "/tech/aws.svg",
+        alt: "aws",
+        key: "aws",
+        className: "h-16",
+    },
+    {
+        src: "/tech/wormhole.svg",
+        alt: "wormhole",
+        key: "wormhole",
+        className: "h-5",
+    },
+    {
+        src: "/tech/google-cloud.svg",
+        alt: "google-cloud",
+        key: "google-cloud",
+        className: "h-10",
+    },
+    {
+        src: "/tech/lifi.svg",
+        alt: "lifi",
+        key: "lifi",
+        className: "h-10",
+    },
+    {
+        src: "/tech/zksync.svg",
+        alt: "zksync",
+        key: "zksync",
+        className: "h-8",
+    },
+]
+
 export const MarqueeTicker = () => {
     return (
-        <Marquee speed={50} gradient={true} gradientColor="black">
-            {images.map(({ src, alt, key, className }) => (
-                <Image
-                    key={key}
-                    className={clsx("h-10 mx-6", className)}
-                    src={src}
-                    alt={alt}
-                    removeWrapper
-                />
-            ))}
-        </Marquee>
+        <div>
+            <Marquee speed={50} gradient={true} gradientColor="black">
+                {imagesTop.map(({ src, alt, key, className }) => (
+                    <Image
+                        key={key}
+                        className={clsx("h-10 mx-6", className)}
+                        src={src}
+                        alt={alt}
+                        radius="none"
+                        removeWrapper
+                    />
+                ))}
+            </Marquee>
+            <Spacer y={6}/>
+            <Marquee speed={50} direction="right" gradient={true} gradientColor="black">
+                {imagesBottom.map(({ src, alt, key, className }) => (
+                    <Image
+                        key={key}
+                        radius="none"
+                        className={clsx("h-10 mx-6", className)}
+                        src={src}
+                        alt={alt}
+                        removeWrapper
+                    />
+                ))}
+            </Marquee>
+        </div>
     )
 }
